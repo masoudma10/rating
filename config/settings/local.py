60702fc7630cf04c82706bdbc8d2b78c4ab0b993
masoudma1o <masoud_mohebali@yahoo.com>
2024-06-07 16:40:51 +0430

    Start project

---
+++ config/settings/local.py
@@ -0,0 +1,43 @@
+from .base import *
+
+
+SECRET_KEY = 'django-insecure-7710xfbykyqi03rn%$!4^yjyge+q@@&$=d95emutdw2)a7)i46'
+DEBUG = True
+ALLOWED_HOSTS = ['*']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'wallet',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+    }
+}
+
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient"
+        },
+        "KEY_PREFIX": os.getenv('CACHE_KEY_PREFIX', 'rating'),
+        "VERSION": 4,
+    },
+
+}
+
+# CELERY_BROKER_URL = os.environ.get('CELERY_BROKER_URL')
+# CELERY_BROKER_URL = 'amqp://guest:guest@rabbitmq:5672//'
+# CELERY_RESULT_BACKEND = 'django-db'
+CELERY_BROKER_URL = 'amqp://guest:guest@127.0.0.1:5672//'
+CELERY_RESULT_BACKEND = 'redis://127.0.0.1:6379/'
+CELERY_ACCEPT_CONTENT = ['application/json']
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_STORE_ERRORS_EVEN_IF_IGNORED = True
+CELERY_DEFAULT_QUEUE = 'default'
+CELERY_TIMEZONE = 'Asia/Tehran'
+CELERY_VISIBILITY_TIMEOUT = 365*86400
---
+++ config/settings/production.py
@@ -0,0 +1,68 @@
+from .base import *
+
+
+SECRET_KEY = os.environ.get('GENERAL_SECRET_KEY')
+DEBUG = False
+ALLOWED_HOSTS = ['*']
+
+CSRF_TRUSTED_ORIGINS = ['']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT'),
+    }
+}
+
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_CREDENTIALS = True
+
+
+CELERY_BROKER_URL = f'amqp://{os.environ.get("USERNAME_RABBITMQ")}:{os.environ.get("PASSWORD_RABBITMQ")}@rabbitmq:5672//'
+CELERY_ACCEPT_CONTENT = ['application/json']
+CELERY_RESULT_SERIALIZER = 'pickle'
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_STORE_ERRORS_EVEN_IF_IGNORED = True
+CELERY_CACHE_BACKEND = 'django-cache'
+CELERY_DEFAULT_QUEUE = 'default'
+CELERY_TIMEZONE = 'Asia/Tehran'
+CELERY_VISIBILITY_TIMEOUT = 365*86400
+
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": [
+            f'redis://{os.environ.get("CACHE_ADDRESS1")}',  # leader
+            f'redis://{os.environ.get("CACHE_ADDRESS2")}',  # read-replica 1
+            f'redis://{os.environ.get("CACHE_ADDRESS3")}',  # read-replica 2
+        ],
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient"
+        },
+        "KEY_PREFIX": os.getenv('CACHE_KEY_PREFIX', 'rating'),
+        "VERSION": 4,
+    },
+
+}
+# REST_FRAMEWORK = {
+#     'DEFAULT_RENDERER_CLASSES': (
+#         'rest_framework.renderers.JSONRenderer',
+#     ),
+#     'DEFAULT_PARSER_CLASSES': (
+#         'rest_framework.parsers.JSONParser',
+#     ),
+#     # 'DEFAULT_AUTHENTICATION_CLASSES': (
+#     #     'rest_framework.authentication.SessionAuthentication',
+#     # )
+# }
+# REST_FRAMEWORK = {
+#  'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema',
+#     'DEFAULT_AUTHENTICATION_CLASSES': [
+#         'rest_framework.authentication.BasicAuthentication',
+#         'rest_framework.authentication.TokenAuthentication',
+#     ],
+# }
